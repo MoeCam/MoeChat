9626627f4fe25a0ff12cfe95e6c8b32b5b2c9c6d
dujiepeng <347302029@qq.com>
2014-05-20 16:33:05 +0800

    修改百度地图appkey

--- ChatDemo-UI/ChatDemo-UI/Class/AppDelegate.m
+++ ChatDemo-UI/ChatDemo-UI/Class/AppDelegate.m
@@ -27,7 +27,7 @@
 #warning Baidu地图SDK测试Key值
     // 如果要关注网络及授权验证事件，请设定generalDelegate参数
     BMKMapManager *mapManager = [[BMKMapManager alloc]init];
-    BOOL ret = [mapManager start:@"akCmCtku7GVde6q2FcCpGo69"  generalDelegate:nil];
+    BOOL ret = [mapManager start:@"6QN0dGpbbrcFXqCpW4F86DeQ"  generalDelegate:nil];
     if (!ret) {
         NSLog(@"manager start failed!");
     }
