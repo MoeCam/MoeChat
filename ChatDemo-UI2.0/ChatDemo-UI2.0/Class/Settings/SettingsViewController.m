2f137e152031906cea7b7f4e4fb6dfb7cfa50b56
xieyajie <xyjxyf@163.com>
2014-05-24 09:23:33 +0800

    设置页面退出按钮

--- ChatDemo-UI2.0/ChatDemo-UI2.0/Class/Settings/SettingsViewController.m
+++ ChatDemo-UI2.0/ChatDemo-UI2.0/Class/Settings/SettingsViewController.m
@@ -8,6 +8,8 @@
 
 #import "SettingsViewController.h"
 
+//#import "EaseMob.h"
+
 @interface SettingsViewController ()
 
 @end
@@ -26,11 +28,34 @@
 {
     [super viewDidLoad];
     self.title = @"设置";
+    
+    UIButton *logoutButton = [[UIButton alloc] initWithFrame:CGRectMake(30, 20, self.view.frame.size.width - 60, 40)];
+    [logoutButton setBackgroundColor:[UIColor whiteColor]];
+    [logoutButton setTitle:@"退出登录" forState:UIControlStateNormal];
+    [logoutButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [logoutButton addTarget:self action:@selector(logoutAction) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:logoutButton];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
+}
+
+#pragma mark - action
+
+- (void)logoutAction
+{
+//    [[EaseMob sharedInstance].chatManager asyncLogoffWithCompletion:^(NSDictionary *info, EMError *error) {
+//        if (error) {
+//            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"警告" message:@"退出当前账号失败，请重新操作" delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
+//            [alertView show];
+//        }
+//        else{
+//            [[DataManager defaultManager] clear];
+//            [[NSNotificationCenter defaultCenter] postNotificationName:KNOTIFICATION_LOGINCHANGE object:@NO];
+//        }
+//    } onQueue:nil];
 }
  
 @end
