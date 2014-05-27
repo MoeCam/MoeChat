492284ff5241eba9144cbc21336a6a4834c2f095
dujiepeng <347302029@qq.com>
2014-05-27 18:18:29 +0800

    update.

--- ChatDemo-UI2.0/ChatDemo-UI2.0/Class/Chat/ChatView/ChatViewCell/ChatViewBubble/EMChatCellLocationBubble.m
+++ ChatDemo-UI2.0/ChatDemo-UI2.0/Class/Chat/ChatView/ChatViewCell/ChatViewBubble/EMChatCellLocationBubble.m
@@ -9,7 +9,8 @@
 #import "EMChatCellLocationBubble.h"
 
 @interface EMChatCellLocationBubble (){
-    UIImageView *_chatImage;
+    UIImageView         *_chatImage;
+    UILabel             *_addressLabel;
 }
 
 @end
@@ -20,19 +21,40 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        _chatImage = [[UIImageView alloc] initWithFrame:CGRectZero];
+        _chatImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, IMAGE_MAX_SIZE,
+                                                                   IMAGE_MAX_SIZE)];
         _chatImage.userInteractionEnabled = NO;
         _chatImage.multipleTouchEnabled = NO;
         [self addSubview:_chatImage];
+        
+        _addressLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, LOCATION_ADDRESS_WIDTH,
+                                                                  LOCATION_ADDRESS_HEIGHT)];
+        _addressLabel.font = [UIFont systemFontOfSize:LOCATION_ADDRESS_FONTSIZE];
+        _addressLabel.textColor = [UIColor whiteColor];
+        [_addressLabel setNumberOfLines:0];
+        [self addSubview:_addressLabel];
     }
     return self;
 }
 
 -(void)layoutSubviews{
     [super layoutSubviews];
-    _chatImage.frame = CGRectMake(0, 0, IMAGE_MAX_SIZE
-                                  , IMAGE_MAX_SIZE);
+    CGRect frame = _chatImage.frame;
+    if (self.model.isLeft) {
+        frame.origin.x = CHATCELL_BUBBLE_WITHIN_PADDING + CHATCELL_BUBBLE_ARROW_WIDTH;
+    }else{
+        frame.origin.x = CHATCELL_BUBBLE_WITHIN_PADDING;
+    }
+    frame.origin.y = CHATCELL_BUBBLE_WITHIN_PADDING;
+    _chatImage.frame = frame;
     [_chatImage setImage:[UIImage imageNamed:LOCATION_DEFAULT_IMAGE]];
+    
+    _addressLabel.text = self.model.address;
+    [_addressLabel sizeToFit];
+    frame = _addressLabel.frame;
+    frame.origin.x = CHATCELL_BUBBLE_WITHIN_PADDING + LOCATION_ADDRESS_SPACING +CHATCELL_BUBBLE_ARROW_WIDTH;
+    frame.origin.y = self.frame.size.height - _addressLabel.frame.size.height - CHATCELL_BUBBLE_WITHIN_PADDING - LOCATION_ADDRESS_SPACING;
+    _addressLabel.frame = frame;
 }
 
 - (CGSize)sizeThatFits:(CGSize)size{
