109fb0bfcc90cddede16937e2fb16296a0f64bf0
dujiepeng <347302029@qq.com>
2014-07-14 15:09:12 +0800

    update

--- ChatDemo-UI2.0/ChatDemo-UI2.0/Category/NSString+Valid.h
+++ ChatDemo-UI2.0/ChatDemo-UI2.0/Category/NSString+Valid.h
@@ -1,10 +1,14 @@
-//
-//  NSString+Valid.h
-//  ChatDemo-UI2.0
-//
-//  Created by dujiepeng on 14-7-13.
-//  Copyright (c) 2014年 dujiepeng. All rights reserved.
-//
+/************************************************************
+ *  * EaseMob CONFIDENTIAL
+ * __________________
+ * Copyright (C) 2013-2014 EaseMob Technologies. All rights reserved.
+ *
+ * NOTICE: All information contained herein is, and remains
+ * the property of EaseMob Technologies.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from EaseMob Technologies.
+ */
 
 #import <Foundation/Foundation.h>
 
