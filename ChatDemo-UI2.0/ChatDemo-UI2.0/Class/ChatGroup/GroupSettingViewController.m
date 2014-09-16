c97eaa53ef1fd0098e53a947de9348820368de53
xieyajie <xyjxyf@163.com>
2014-09-16 10:33:31 +0800

    block group

--- ChatDemo-UI2.0/ChatDemo-UI2.0/Class/ChatGroup/GroupSettingViewController.m
+++ ChatDemo-UI2.0/ChatDemo-UI2.0/Class/ChatGroup/GroupSettingViewController.m
@@ -11,7 +11,9 @@
 @interface GroupSettingViewController ()
 {
     EMGroup *_group;
-    UISwitch *_switch;
+    BOOL _isOwner;
+    UISwitch *_pushSwitch;
+    UISwitch *_blockSwitch;
 }
 
 @end
@@ -32,6 +34,10 @@
     self = [self initWithStyle:UITableViewStylePlain];
     if (self) {
         _group = group;
+        
+        NSDictionary *loginInfo = [[[EaseMob sharedInstance] chatManager] loginInfo];
+        NSString *loginUsername = [loginInfo objectForKey:kSDKUsername];
+        _isOwner = [_group.owners containsObject:loginUsername];
     }
     
     return self;
@@ -49,7 +55,21 @@
     UIBarButtonItem *backItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
     [self.navigationItem setLeftBarButtonItem:backItem];
     
+    if (!_isOwner) {
+        UIBarButtonItem *saveItem = [[UIBarButtonItem alloc] initWithTitle:@"保存" style:UIBarButtonItemStylePlain target:self action:@selector(saveAction:)];
+        [self.navigationItem setRightBarButtonItem:saveItem];
+    }
+    
+    _pushSwitch = [[UISwitch alloc] init];
+    [_pushSwitch addTarget:self action:@selector(pushSwitchChanged:) forControlEvents:UIControlEventValueChanged];
+    [_pushSwitch setOn:_group.isPushNotificationEnabled animated:YES];
+    
+    _blockSwitch = [[UISwitch alloc] init];
+    [_blockSwitch addTarget:self action:@selector(blockSwitchChanged:) forControlEvents:UIControlEventValueChanged];
+    [_blockSwitch setOn:_group.isBlocked animated:YES];
+    
     self.tableView.tableFooterView = [[UIView alloc] init];
+    [self.tableView reloadData];
 }
 
 - (void)didReceiveMemoryWarning
@@ -69,7 +89,17 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    return 1;
+    if (_isOwner) {
+        return 1;
+    }
+    else{
+        if (_blockSwitch.isOn) {
+            return 1;
+        }
+        else{
+            return 2;
+        }
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -81,24 +111,25 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
     }
     
-    if (indexPath.row == 0) {
-        if (_switch == nil) {
-            _switch = [[UISwitch alloc] init];
-            _switch.frame = CGRectMake(self.tableView.frame.size.width - (_switch.frame.size.width + 10), (cell.contentView.frame.size.height - _switch.frame.size.height) / 2, _switch.frame.size.width, _switch.frame.size.height);
-            [_switch addTarget:self action:@selector(switchChanged:) forControlEvents:UIControlEventValueChanged];
-        }
-        BOOL isOn = _group.isPushNotificationEnabled;
-        [_switch setOn:isOn animated:YES];
+    if ((_isOwner && indexPath.row == 0) || (!_isOwner && indexPath.row == 1)) {
+        _pushSwitch.frame = CGRectMake(self.tableView.frame.size.width - (_pushSwitch.frame.size.width + 10), (cell.contentView.frame.size.height - _pushSwitch.frame.size.height) / 2, _pushSwitch.frame.size.width, _pushSwitch.frame.size.height);
         
-        if (isOn) {
+        if (_pushSwitch.isOn) {
             cell.textLabel.text = @"接收并提示群消息";
         }
         else{
             cell.textLabel.text = @"只接收不提示群消息";
         }
         
-        [cell.contentView addSubview:_switch];
-        [cell.contentView bringSubviewToFront:_switch];
+        [cell.contentView addSubview:_pushSwitch];
+        [cell.contentView bringSubviewToFront:_pushSwitch];
+    }
+    else if(!_isOwner && indexPath.row == 0){
+        _blockSwitch.frame = CGRectMake(self.tableView.frame.size.width - (_blockSwitch.frame.size.width + 10), (cell.contentView.frame.size.height - _blockSwitch.frame.size.height) / 2, _blockSwitch.frame.size.width, _blockSwitch.frame.size.height);
+        
+        cell.textLabel.text = @"屏蔽群消息";
+        [cell.contentView addSubview:_blockSwitch];
+        [cell.contentView bringSubviewToFront:_blockSwitch];
     }
     
     return cell;
@@ -114,12 +145,6 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    
-    if (indexPath.section == 0) {
-        if (indexPath.row == 0) {
-            [self switchChanged:_switch];
-        }
-    }
 }
 
 #pragma mark - private
@@ -132,7 +157,7 @@
         [weakSelf hideHud];
         if (!error) {
             [weakSelf showHint:@"设置成功"];
-            [weakSelf.tableView reloadData];
+//            [weakSelf.tableView reloadData];
         }
         else{
             [weakSelf showHint:@"设置失败"];
@@ -147,10 +172,41 @@
     [self.navigationController popViewControllerAnimated:YES];
 }
 
-- (void)switchChanged:(id)sender
+- (void)pushSwitchChanged:(id)sender
 {
-    BOOL toOn = _switch.isOn;
-    [self isIgnoreGroup:!toOn];
+    if (_isOwner) {
+        BOOL toOn = _pushSwitch.isOn;
+        [self isIgnoreGroup:!toOn];
+    }
+}
+
+- (void)blockSwitchChanged:(id)sender
+{
+    [self.tableView reloadData];
+}
+
+- (void)saveAction:(id)sender
+{
+    if (_blockSwitch.isOn != _group.isBlocked) {
+        __weak typeof(self) weakSelf = self;
+        [self showHudInView:self.view hint:@"设置属性"];
+        if (_blockSwitch.isOn) {
+            [[EaseMob sharedInstance].chatManager asyncBlockGroup:_group.groupId completion:^(EMGroup *group, EMError *error) {
+                [weakSelf hideHud];
+                [weakSelf showHint:@"设置成功"];
+            } onQueue:nil];
+        }
+        else{
+            [[EaseMob sharedInstance].chatManager asyncUnblockGroup:_group.groupId completion:^(EMGroup *group, EMError *error) {
+                [weakSelf hideHud];
+                [weakSelf showHint:@"设置成功"];
+            } onQueue:nil];
+        }
+    }
+    
+    if (_pushSwitch.isOn != _group.isPushNotificationEnabled) {
+        [self isIgnoreGroup:_pushSwitch.isOn];
+    }
 }
 
 @end
