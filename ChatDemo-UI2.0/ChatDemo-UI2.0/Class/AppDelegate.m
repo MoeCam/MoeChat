be7dbac0e04da38505bb5a462787a3e1b19bbc3f
Liu Lantao <liulantao@gmail.com>
2014-10-18 21:24:43 +0800

    更新中文名

--- ChatDemo-UI2.0/ChatDemo-UI2.0/Class/AppDelegate.m
+++ ChatDemo-UI2.0/ChatDemo-UI2.0/Class/AppDelegate.m
@@ -28,7 +28,7 @@
                                                object:nil];
     
     if ([UIDevice currentDevice].systemVersion.floatValue >= 7.0) {
-        [[UINavigationBar appearance] setBarTintColor:RGBACOLOR(78, 188, 211, 1)];
+        [[UINavigationBar appearance] setBarTintColor:RGBACOLOR(34, 178, 111, 1)];
         [[UINavigationBar appearance] setTitleTextAttributes:
          [NSDictionary dictionaryWithObjectsAndKeys:RGBACOLOR(245, 245, 245, 1), NSForegroundColorAttributeName, [UIFont fontWithName:@ "HelveticaNeue-CondensedBlack" size:21.0], NSFontAttributeName, nil]];
     }
@@ -282,7 +282,7 @@
         _mainController = nil;
         LoginViewController *loginController = [[LoginViewController alloc] init];
         nav = [[UINavigationController alloc] initWithRootViewController:loginController];
-        loginController.title = @"萌照";
+        loginController.title = @"萌聊";
     }
 
     if ([UIDevice currentDevice].systemVersion.floatValue < 7.0){
